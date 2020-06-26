2922b86818e4db8e881f62997be0c505cd471917
loverofMush <tolujamal1@gmail.com>
2020-06-26 05:40:10 +0100

    used newproductrequest

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\NewProductRequest;
 use App\Http\Resources\ProductResource;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 use Illuminate\Support\Facades\Gate;
