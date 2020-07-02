300d1c4d5397456107024a31cac776bdc78d3a20
loverofMush <tolujamal1@gmail.com>
2020-07-03 00:18:19 +0100

    changed app route

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://inventreed-demo.herokuapp.com'),
+    'url' => env('APP_URL', 'https://inventreed-api.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
