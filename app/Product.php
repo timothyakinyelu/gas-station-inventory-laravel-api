e1b98194127ea474bb16c33a61d4d89e367e90e3
loverofMush <tolujamal1@gmail.com>
2020-07-08 15:06:27 +0100

    added company_id to fillable array

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'product_type_id', 'product_code_id', 'price'];
+    protected $fillable = ['name', 'company_id', 'product_type_id', 'product_code_id', 'price'];
     public $timestamps = false;
     
     //Relationship
