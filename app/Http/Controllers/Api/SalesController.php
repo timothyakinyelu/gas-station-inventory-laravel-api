0f165d82b23bec2f42c7aeb4241e5b1d7433eaaf
loverofMush <tolujamal1@gmail.com>
2020-07-08 17:21:19 +0100

    .

--- app/Http/Controllers/Api/SalesController.php
+++ app/Http/Controllers/Api/SalesController.php
@@ -218,8 +218,7 @@ class SalesController extends Controller
     public function storeWet(Request $request) 
     {
         $response = Gate::inspect('create', [Sale::class]);
-        print($request->all());
-
+        dd($request->all());
         $foreCourts = json_decode($request->foreCourts, true);
         $List = array();
 
